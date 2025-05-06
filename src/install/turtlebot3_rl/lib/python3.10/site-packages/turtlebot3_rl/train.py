a6484ff7ef6fc92eba685abf8e3d0af326f83a96
Emma Topolovec <49378460+EmmaTopolovec@users.noreply.github.com>
2025-05-06 11:17:13 -0400

    Delete src/install directory

--- src/install/turtlebot3_rl/lib/python3.10/site-packages/turtlebot3_rl/train.py
+++
@@ -1,9 +0,0 @@
-from turtlebot3_env import TurtleBot3Env
-from stable_baselines3 import PPO
-
-if __name__ == "__main__":
-    env = TurtleBot3Env()
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=10000)
-    env.close()
-
